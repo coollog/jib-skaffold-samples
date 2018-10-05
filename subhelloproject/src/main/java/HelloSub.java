891e29bdce9599569413fb83f204a48a10e2b7f0
Qingyang Chen <qingyangc@google.com>
2018-10-05 14:52:10 -0400

    Set up projects.

---
+++ subhelloproject/src/main/java/HelloSub.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import static spark.Spark.get;
+
+public class HelloSub {
+
+  // public static void main(String[] args) {
+  //   System.out.println("hello sub 4");
+  //   try {
+  //     Thread.sleep(4000);
+  //   } catch (InterruptedException e) {
+  //     e.printStackTrace();
+  //   }
+  // }
+
+  public static void main(String[] args) {
+    get("/name", (req, res) -> {
+      System.out.println("Got /name");
+      return "kasfjwenf";
+    });
+  }
+}
