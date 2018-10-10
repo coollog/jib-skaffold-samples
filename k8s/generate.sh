35e76119f8996649858166d4765b1ad7e44e7c6b
Qingyang Chen <qingyangc@google.com>
2018-10-10 12:11:48 -0400

    Updates generate.sh.

--- k8s/generate.sh
+++ k8s/generate.sh
@@ -1,11 +1,26 @@
 #! /bin/sh
 
-kubectl run helloworld --image=gcr.io/qingyangc-sandbox/skaffold-jib --port=8080 -o yaml --dry-run > helloworld.yaml
+set -ex
 
-# This is a complete hack.
-kubectl run helloworld-sub --image=gcr.io/qingyangc-sandbox/skaffold-sub-jib --port=5005 -o yaml --dry-run > temp1
-kubectl run helloworld-sub --image=gcr.io/qingyangc-sandbox/skaffold-sub-jib --port=4567 -o yaml --dry-run > temp2
-diff -Dignore temp1 temp2 | grep -v '^#if' | grep -v '^#else' | grep -v '^#endif' > helloworld-sub.yaml
+HELLO_SERVICE_YAML=hello-service.yaml
+NAME_SERVICE_YAML=name-service.yaml
+
+HELLO_SERVICE_IMAGE=gcr.io/qingyangc-sandbox/skaffold-jib-hello-service
+NAME_SERVICE_IMAGE=gcr.io/qingyangc-sandbox/skaffold-jib-name-service
+
+# Generates the deployment for the hello-service, exposing port 8080 (HTTP).
+echo "# AUTO-GENERATED" > ${HELLO_SERVICE_YAML}
+kubectl run hello-service --image=${HELLO_SERVICE_IMAGE} --port=8080 -o yaml --dry-run >> ${HELLO_SERVICE_YAML}
+
+# Generates the deployment for the name-service, exposing ports 4567 (HTTP) and 5005 (debugger)
+kubectl run name-service --image=${NAME_SERVICE_IMAGE} --port=5005 -o yaml --dry-run > temp1
+kubectl run name-service --image=${NAME_SERVICE_IMAGE} --port=4567 -o yaml --dry-run > temp2
+echo "# AUTO-GENERATED" > ${NAME_SERVICE_YAML}
+diff -Dignore temp1 temp2 | grep -v '^#if' | grep -v '^#else' | grep -v '^#endif' >> ${NAME_SERVICE_YAML}
 rm temp1 temp2
 
-kubectl expose -f helloworld-sub.yaml --port=80 --target-port=4567 --name=name-service --type=ClusterIP --dry-run -o yaml > name-service.yaml
+# Generates the service for the name-service.
+kubectl expose -f ${NAME_SERVICE_YAML} --port=80 --target-port=4567 --name=name-service --type=ClusterIP --dry-run -o yaml >> temp
+echo "---" >> ${NAME_SERVICE_YAML}
+cat temp >> ${NAME_SERVICE_YAML}
+rm temp
